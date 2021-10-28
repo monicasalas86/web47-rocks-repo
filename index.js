b5f24f2de7194230e6ae8c7461c489cec91824cb
monicasalas86 <monicsalas167@gmail.com>
2021-10-28 12:42:12 -0500

    made cra

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
 require('dotenv').config()
-
+const path = require('path')
 const express = require('express')
 const app = express()
-
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'client/build'))) //absolute path to build folder inside client
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send(`
-    <h1>Web ${process.env.COHORT} rocks!</h1>
-    `)
+app.get('/api/foo', (req, res) => {
+    res.json({message: 'hello there'})
 })
-
-app.get('/port', (req, res) => {
-    res.send(`
-    <div>port is ${PORT}</div>
-    `)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
-
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 })
-
-
-
-function cohortRocks() {
-    console.log(`web ${process.argv[2]} rocks`) //fix it using process.argv!!
-}
-
-function cohortRocksEnv() {
-    console.log(`home folder is "${process.env.HOME}"`)
-    console.log(`my shell is "${process.env.SHELL}"`)
-    console.log(`in my environment FOO is "${process.env.FOO}"`)
-}
-cohortRocksEnv()
