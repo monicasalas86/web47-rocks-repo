76ce6004ec7f1970c446c6d16277e554dff6edc0
monicasalas86 <monicsalas167@gmail.com>
2021-10-28 11:56:21 -0500

    wip

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
@@ -6,6 +8,12 @@ const PORT = process.env.PORT || 5000
 app.get('/', (req, res) => {
     res.send(`
     <h1>Web 47 rocks!</h1>
+    `)
+})
+
+app.get('/port', (req, res) => {
+    res.send(`
+    <div>port is ${PORT}</div>
     `)
 })
 
